961b1b74aa1fea33f3909ffe1f912f7477246db1
Skrier <alex.lewis79@yahoo.co.uk>
2014-07-31 22:05:54 +0100

    bb

--- js/app.js
+++ js/app.js
@@ -29,50 +29,53 @@ $(document).ready(function(){
           var variance = Math.abs(inputGuess - randomNom);
           if (inputGuess <= 0) {
              alert('You have not entered in a correct number. Please enter a number greater the 0.');
-          } else if (variance >= 50) {
+          }  
+            else if (randomNo === inputGuess) {
+             $('#feedback').text("You Guessed the correct number " + inputGuess);
+           }
+            else if (variance >= 50) {
              $('#feedback').text("Rigor Mortis!");
            }
-           else if (variance < 50 && variance >= 35) {
+           else if (variance >= 35) {
              $('#feedback').text("Very Cold");
            }
-           else if (variance < 35 && variance >= 25) {
+           else if (variance >= 25) {
              $('#feedback').text("Cold");
            }
-           else if (variance < 25 && variance >= 15) {
+           else if (variance >= 15) {
              $('#feedback').text("Warm");
            }
-           else if (variance < 15 && variance >= 10) {
+           else if (variance >= 10) {
              $('#feedback').text("Hot");
            }
-           else if (variance < 10 && variance >= 5) {
+           else if (variance >= 5) {
              $('#feedback').text("Very Hot");
            }
-           else if (variance < 5 && variance >= 1) {
+           else if (variance >= 0) {
              $('#feedback').text("Incandescent");
            } 
            else {
-           	 $('#feedback').text("You Guessed the correct number " + inputGuess);
+           	 alert('the game is broken');
            };
     };
 
      /*--- Variable that appends the guess to the <ul id = guessList> ---*/
-     var listGuess = "<li>"+ inputGuess +"</li>";
+    var listGuess = "<li>"+ inputGuess +"</li>";
 
+    var namedFunction = function () {$('#guessList').append(listGuess);
+        	$('#count').text(countGuess);
+	        runGame();
+    }
      /*--- Variable that counts the number of Guesses ---*/
-     var countGuess = $(".guessBox li").length;
-
+     var countGuess = function () {
+     	$(".guessBox li").length;
+     }
      /*--- jQuery Events that allow you to play the game ---*/
-    $("#guessButton").mousedown(function () {
-            $('#count').text(countGuess);
-        	$('#guessList').append(listGuess);
-        	runGame();   	
-    });
+    $("#guessButton").mousedown(namedFunction);
 
     $("#userGuess").keydown(function (enter) {
         	if(enter.which === 13) {
-        	$('#guessList').append(listGuess);
-        	$('#count').text(countGuess);
-	        runGame();
+        	namedFunction();
 	        }
     });
 
